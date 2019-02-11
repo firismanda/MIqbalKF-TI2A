ec598ff88419be535e35c73d113e0fc64537fd43
firismanda <ryuka.1111@gmail.com>
2019-02-11 15:58:36 +0700

    tugas 2

---
+++ src/app/components/bio/bio.component.ts
@@ -0,0 +1,29 @@
+import { BioService } from './../../services/bio.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bio',
+  templateUrl: './bio.component.html',
+  styleUrls: ['./bio.component.css']
+})
+export class BioComponent implements OnInit {
+
+  Title: String;
+  Tanggal: any;
+  Hobbies: any[];
+  Nama: String;
+  Alamat: String;
+
+  constructor(private bioServ:BioService) { }
+
+  ngOnInit() {
+    this.Title = "INI Tugas Pertama Biodata";
+    this.Tanggal = Date.now();
+    this.Nama = "Muhammad Iqbal Kharis Firismanda";
+    this.Alamat = "Jln. Janti Barat C/B.25";
+    this.Hobbies = this.bioServ.getHobby();
+  }
+
+
+
+}
